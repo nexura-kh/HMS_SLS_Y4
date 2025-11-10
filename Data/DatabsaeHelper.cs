5e49e68c0c41c69989fa9d831f0d60bfa7d08d5b
12chenkong <channkong582@gmail.com>
2025-11-10 23:30:27 +0700

    updated README file

--- Data/DatabsaeHelper.cs
+++ Data/DatabsaeHelper.cs
@@ -9,7 +9,7 @@ namespace HMS_SLS_Y4.Data
     public static class DatabaseHelper
     {
         public static string ConnectionString =>
-            "Server=127.0.0.1;Port=3306;Database=hotel_db;Uid=root;Pwd=root;";
+            "Server=127.0.0.1;Port=3308;Database=hotel_db;Uid=root;Pwd=root;";
     }
 
     
